7742c682c7c7d65a624d62c356e954162cfcba61
Jake Zim <sad2project@gmail.com>
2022-10-12 14:29:53 -0500

    Fixed Typo in setup.py

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['pyimmatcher', 'pyimmatcher.api', 'pyimmatcher.basic'],
     package_dir={'': 'src'},
     url='',
-    license='Free and Opne',
+    license='Free and Open',
     author='Jacob Zimmerman',
     author_email='jacobz_20@yahoo.com',
     description='Python version of immatcher, a testing matcher library based on immutable matchers (called Assertions) returning a TestResult rather than being polled for each step, like Hamcrest.'
