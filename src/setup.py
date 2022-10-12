286ad4e529585a3305f141fe8a8c5728af09c49b
Jake Zim <sad2project@gmail.com>
2022-10-12 14:01:48 -0500

    Added setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pyimmatcher',
+    version='1.0',
+    packages=['pyimmatcher', 'pyimmatcher.api', 'pyimmatcher.basic'],
+    package_dir={'': 'src'},
+    url='',
+    license='Free and Opne',
+    author='Jacob Zimmerman',
+    author_email='jacobz_20@yahoo.com',
+    description='Python version of immatcher, a testing matcher library based on immutable matchers (called Assertions) returning a TestResult rather than being polled for each step, like Hamcrest.'
+)
